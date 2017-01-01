ab57737a1e78e6d7dbc1eef7714dd3a8bc7e0829
David Rolfe <drolfe@voltdb.com>
2017-01-01 13:17:57 +0000

    fix ubuntu

--- setup_server.sh
+++ setup_server.sh
@@ -173,7 +173,7 @@ do
        then
               echo ${XS} Creating ${i}... ${XE}
               curl https://raw.githubusercontent.com/srmadscience/voltdb-cloudformation/master/${i}  > /home/ubuntu/${i}
-              chown /home/ubuntu/ubuntu ${i}
+              chown ubuntu ${i}
               chmod /home/ubuntu/755 ${i}
        fi
 done
