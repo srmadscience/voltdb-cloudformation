527a4b1d3b27ec714ee3780a70d0e2d6c2113c3b
David Rolfe <drolfe@voltdb.com>
2017-01-02 18:36:47 +0000

    Create check_bandwidth.sh

---
+++ check_bandwidth.sh
@@ -0,0 +1,17 @@
+!/bin/sh
+
+ITYPE=`ec2metadata --instance-type`
+SECS=30
+INT=1
+TRANSIZE=256
+
+echo Instance Type = $ITYPE 
+echo Spending $SECS seconds testing bandwidth
+
+FNAME=check_bandwidth_${ITYPE}_`date '+%y%m%d_%H%M%S'.lst`
+iperf3 -c 54.82.218.143 -f K -t $SECS -i $INT | tee -a $FNAME
+RECS=`cat $FNAME | awk '{ print $7}' | grep '^[0-9]' | tail -n 1`
+RECS=`expr $RECS \* 1024`
+RECS=`expr $RECS / $TRANSIZE `
+
+echo Maximum possible ${TRANSIZE} byte transactions supperted by this nerwork each second = $RECS
