098b14528ef172e8e6ae06a410177f37c02b03b9
David Rolfe <drolfe@voltdb.com>
2017-01-03 15:29:47 +0000

    Update update_for_cluster.sh

--- update_for_cluster.sh
+++ update_for_cluster.sh
@@ -24,6 +24,7 @@
 
 . /home/ubuntu/.profile
 
+cd /home/ubuntu
 find voltdb-pro/examples -name run.sh -print -exec /home/ubuntu/update_example_hosts.sh {} \;
 find voltdb-pro/examples -name  '*Benchmark.java' -print -exec /home/ubuntu/update_java_clients.sh  {} \;
 
